5924f273379ea61fcf82d206a644b1d02abf97f5
CherenChin <60425620+CherenChin@users.noreply.github.com>
2020-03-14 17:12:45 +0000

    Update adc.cpp

--- Electronics/adc.cpp
+++ Electronics/adc.cpp
@@ -1,49 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <cmath>
 
 #include "Adafruit_ADS1015.h"
 #include <wiringPiI2C.h>
 
-// CLASS FOR CONVERTING ANALOG INPUT ADC VALUES TO VOLTAGE (NOT WORKING)
-
-Adafruit_ADS1015 ads;
-
-//Convert Two's complement to Hex
-char convert(size_t const size, void const * const ptr)
-{
-  unsigned char *b = (unsigned char*) ptr;
-  unsigned char byte;
-  int i, j, hex;
-  
-  // Conversion to Binary
-  for (i=size-1;i>=0;i--) {
-    for (j=7;j>=0;j--) {
-      byte = (b[i] >> j) & 1;
-      printf("%u", byte);
-    }
-  }
-  
-  // Conversion to Hex
-  
-  
-  return hex;
-}
-
-
 int main(int argc, char *argv[])
 {
   uint16_t adc0;
-  int hex, dec, vol;
+  float conversion, voltage;
 
   ads.setGain(GAIN_ONE);
   ads.begin();
   while (true) {
     adc0 = ads.readADC_SingleEnded(0);
-    hex = convert(sizeof(adc0), &adc0);
-    dec = hex % 10;
-    vol = dec*3300/4096;
-    printf(" *** %d\n", vol);
+    conversion = 3.3/4096;
+    voltage = int(adc0)*conversion;
+    printf("%f\n", voltage);
     usleep(100000);
   }
 }
